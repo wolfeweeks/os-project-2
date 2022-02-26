1bdc8bcfa6a96a38e232238a7bdd050671aea8a9
Wolfe Weeks <wwwkf9@umsystem.edu>
2022-02-26 16:44:42 -0600

    added functions for shared memory management

--- master.c
+++ master.c
@@ -1,8 +1,74 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h> //for getopt variables (e.g. opterr, optopt, etc.)
+#include <unistd.h>
 #include "config.h"
+#include "sharedMemory.h"
 
 int main(int argc, char *argv[])
 {
-  printf("%d", MAX_CHILDREN);
+  int numOfProcs;
+  int maxTime = 100;
+
+  int options = 0;
+
+  // read through the user's command line options and set any necessary args
+  while (options != -1)
+  {
+    options = getopt(argc, argv, "ht:");
+    switch (options)
+    {
+    case 'h':
+      printf("usage: master -t maxSeconds numOfProcs\n");
+      return 0;
+    case 't':
+      maxTime = atoi(optarg);
+      break;
+    case ':':
+      printf("Option needs a value\n");
+      return 1;
+    case '?':
+      printf("%c is not an option\n", optopt);
+      return 1;
+    default:
+      break;
+    }
+  }
+
+  if (argc < 2 || argc > 4) // ensure proper number of args
+  {
+    fprintf(stderr, "usage: master -t maxSeconds numOfProcs\n");
+    exit(1);
+  }
+
+  // set number of processes to the user entered amount or 19
+  if (argc == 2)
+  {
+    if (atoi(argv[1]) > MAX_CHILDREN)
+    {
+      numOfProcs = MAX_CHILDREN;
+    }
+    else
+    {
+      numOfProcs = atoi(argv[1]);
+    }
+  }
+  else
+  {
+    if (atoi(argv[3]) > MAX_CHILDREN)
+    {
+      numOfProcs = MAX_CHILDREN;
+    }
+    else
+    {
+      numOfProcs = atoi(argv[3]);
+    }
+  }
+
+  if (fork() == 0)
+  {
+    break;
+  }
+
   return 0;
 }
